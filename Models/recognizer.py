5230b1a4afee07333d35ce3a81c54ff82f999f39
Morojs <moro.hicham@emsi-edu.ma>
2021-02-20 01:31:35 +0100

    add recognizer class

---
+++ Models/recognizer.py
@@ -0,0 +1,52 @@
+import speech_recognition as sr
+from playsound import playsound
+class Recognizer :
+
+    def __init__(self,recognizer, microphone) : 
+        self.recognizer=recognizer
+        self.microphone=microphone
+    
+    def recognize_speech_from_mic(self):
+        """Transcribe speech from recorded from `microphone`.
+        Returns a dictionary with three keys:
+        "success": a boolean indicating whether or not the API request was
+               successful
+        "error":   `None` if no error occured, otherwise a string containing
+               an error message if the API could not be reached or
+               speech was unrecognizable
+        "transcription": `None` if speech could not be transcribed,
+               otherwise a string containing the transcribed text
+        """
+        playsound('sounds/start.wav')
+        # check that recognizer and microphone arguments are appropriate type
+        if not isinstance(self.recognizer, sr.Recognizer) :
+            raise TypeError("`recognizer` must be `Recognizer` instance")
+ 
+        if not isinstance(self.microphone, sr.Microphone) :
+            raise TypeError("`microphone` must be `Microphone` instance")
+
+        # adjust the recognizer sensitivity to ambient noise and record audio
+        # from the microphone
+        with self.microphone as source:
+            self.recognizer.adjust_for_ambient_noise(source)
+            audio = self.recognizer.listen(source)
+
+        # set up the response object
+        response = {
+            "success": True,
+            "error": None,
+            "transcription": None
+        }
+        # try recognizing the speech in the recording
+        # if a RequestError or UnknownValueError exception is caught,
+        #     update the response object accordingly
+        try:
+            response["transcription"] = self.recognizer.recognize_google(audio)
+        except sr.RequestError:
+            # API was unreachable or unresponsive
+            response["success"] = False
+            response["error"] = "API unavailable"
+        except sr.UnknownValueError:
+            # speech was unintelligible
+            response["error"] = "Unable to recognize speech"
+        return response
