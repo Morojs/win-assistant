0950bc3cdb52b2bce0494d00980a90d91cbb1177
Morojs <moro.hicham@emsi-edu.ma>
2021-02-28 19:03:06 +0100

    fix file not found error: [winerror 2]

--- helpers/file_process.py
+++ helpers/file_process.py
@@ -1,51 +1,55 @@
 from pathlib import Path
-import os,subprocess,platform
+import os,subprocess as sp,platform ,shutil
 class FileProcess() :
     
     def __init__(self,input) :
-        self.fileName=input[2]
         #self.filePath=input[3]
-        self.isFile = os.path.isfile("./"+input[2]) 
-        self.fileType=input[1]
         self.key=input[0]
-        self.success=False
+        self.fileType=input[1]
+        self.fileName=input[2]
+        self.isFile = os.path.isfile(self.fileName) 
 
     def request_process(self): 
-        switcher = { 
-            "create": self.create("w"), 
-            "delete": self.delete(), 
-            "make"  : self.create("w"), 
-            "open"  : self.open(),
-            "read"  : self.open()
-        } 
-  
+        if self.key in ["create","make"] :
+            return self.create("w")
+        if self.key=="delete":
+            return self.delete()
+        if self.key in ["open","read"] :
+            return self.open()
     # get() method of dictionary data type returns  
     # value of passed argument if it is present  
     # in dictionary otherwise second argument will 
     # be assigned as default value of passed argument
-        return switcher.get(self.fileType, self.success) 
+        return False
 
     def create(self,mode) :
         if self.fileType=="file" and self.isFile==False :
-            with open("./"+self.fileName,mode) :
-                self.success=True
+            with open(self.fileName,mode) :
+                return True
         else :
             if self.fileType=="folder" and self.isFile==False :
-                os.mkdir("./"+self.fileName)
-                self.success=True
-        return self.success
+                os.mkdir(self.fileName)
+                return True
+        return False
 
     def delete(self) : 
-        if self.fileType=="file" or self.fileType=="folder" and self.isFile==False  :
-             os.remove("./"+self.fileName)
-             self.success=True
-        return self.success
+        if self.isFile==True :
+            os.remove(self.fileName)
+            return True
+        elif self.fileType=="folder" : 
+            shutil.rmtree(self.fileName)
+            return True
+        return False
 
     def open(self) : 
-        if self.fileType=="file" or self.fileType=="folder" and self.isFile==False  :
+        if self.fileType=="file" and self.isFile==True   :
             if platform.system() == 'Windows':    # Windows
-                os.startfile("./"+self.fileName)
-                self.success=True
-        return self.success
+                programName = "notepad.exe"
+                sp.Popen([programName, self.fileName])
+                return True
+        else :
+            os.startfile(self.fileName)
+            return True
+        return False
 
         
